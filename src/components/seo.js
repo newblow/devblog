e0e13003a3d0f6855eea14308162f1342df0b65c
newblow <bollogmx@gmail.com>
2023-12-07 03:23:14 +0100

    update4

--- src/components/seo.js
+++ src/components/seo.js
@@ -35,11 +35,6 @@ const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
         </ul>
       </nav>
       <main>
